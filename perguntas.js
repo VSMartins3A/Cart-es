0a92508f7c845538e1022cda3997b71031348433
VSMartins3A <0000110648972XSP@al.educacao.sp.gov.br>
2024-11-14 05:34:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico com o símbolo "O',
+    "Oxigenio"
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores um time de futebol tem em campo?',
+    '11 jogadores.'
+)
